76a57c1c4a1b19d8307a461e8d5f012c929e3008
Alberto Perdomo <alberto.perdomo@datadope.io>
2021-09-10 18:04:19 +0300

    Build the architecture.

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# sinus_gan
-GAN for a really simple dataset. 
+# GAN for generating sin(x) data points from 0 to 2$\pi$.
+
+Generative Adversarial Networks are a form of neural network architecture in which a discriminator and a generator contest with each other in a game. The architecture learns to generate new data with the same probability distribution as the training set. 
+
+The generator network creates new data candidates, which are later evaludated by the discriminator. The generator or generative network learns to map the latent space to a data distribution of interest, while the discriminator or discriminative network evaluates the candidates produced by the generator and compares them to the true data distribution.
+
+## Architecture 
+In this case, the generator defined by `Discriminator()` class, has a two dimensional input, while the first hidden layer is composed by 256 neurons with ReLU activation. The second and third hidden layers of the generator are composed of 128 and 64 neurons respectively, with again ReLU activation. Finally, the output is composed by a single neuron with activation based on the Sigmoid function. All the three hidden layers use dropout to avoid overfitting.
+
+On the other hand, the discriminator defined by `Generator()` class, has a two dimensional input, but in this case the first hidden layer is composed by 16 neurons. The second hidden layer is formed by 32 neurons, while the output layer is composed by two neurons. In this case, there is no dropout and all the layers used ReLU activation.
+
+## Run 
+By simply running the `main.py` file found in this repo, each epoch is plotted to see the evolution of the learnign process. 
+
+### Requirements
+```
+python3
+pytorch >= 1.4.0
+matplotlib >= 3.3.3
+```
---
+++ main.py
@@ -0,0 +1,118 @@
+import torch
+from torch import nn
+
+import math
+import matplotlib.pyplot as plt
+from torch.nn.modules import loss
+
+torch.manual_seed(111)
+
+# prepare the training data
+train_data_length = 1024
+train_data = torch.zeros((train_data_length, 2))
+train_data[:, 0] = 2*math.pi*torch.rand(train_data_length)
+train_data[:, 1] = torch.sin(train_data[:,0])
+train_labels = torch.zeros(train_data_length)
+train_set = [(train_data[i], train_labels[i]) for i in range(train_data_length)]
+
+plt.plot(train_data[:, 0], train_data[:, 1], ".")
+
+# create pytorch data loader: this shuffles the data from train_set and return it in batches of 32 samples that will be use for nn training
+batch_size = 32
+train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True)
+
+# discriminator 
+class Discriminator(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.model = nn.Sequential(
+            nn.Linear(2, 256),
+            nn.ReLU(),
+            nn.Dropout(.3), 
+            nn.Linear(256, 128), 
+            nn.ReLU(), 
+            nn.Dropout(.3),
+            nn.Linear(128, 64),
+            nn.ReLU(),
+            nn.Dropout(.3),
+            nn.Linear(64, 1),
+            nn.Sigmoid(),
+        )
+    
+    def forward(self, x):
+        output = self.model(x)
+        return output
+
+discriminator = Discriminator()
+
+# generator
+class Generator(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.model = nn.Sequential(
+            nn.Linear(2, 16),
+            nn.ReLU(),
+            nn.Linear(16, 32),
+            nn.ReLU(),
+            nn.Linear(32, 2)
+        )
+    
+    def forward(self, x):
+        output = self.model(x)
+        return output
+
+generator = Generator()
+
+learning_rate = .001
+epochs = 500
+loss_function = nn.BCELoss() # binary cross entropy
+
+# use Adam algorithm to train the model
+optimizer_discriminator = torch.optim.Adam(discriminator.parameters(), lr=learning_rate)
+optimizer_generator = torch.optim.Adam(generator.parameters(), lr=learning_rate)
+
+for epoch in range(epochs):
+    for n, (real_samples, _) in enumerate(train_loader):
+        # data for training the discriminator
+        real_samples_labels = torch.ones((batch_size, 1))
+        latent_space_samples = torch.randn((batch_size, 2))
+        generated_samples = generator(latent_space_samples)
+        generated_samples_labels = torch.zeros((batch_size, 1))
+        all_samples = torch.cat((real_samples, generated_samples))
+        all_samples_labels = torch.cat((real_samples_labels, generated_samples_labels))
+
+        # training the discriminator
+        discriminator.zero_grad()
+        output_discriminator = discriminator(all_samples)
+        loss_discriminator = loss_function(output_discriminator, all_samples_labels)
+        loss_discriminator.backward()
+        optimizer_discriminator.step()
+
+        # data for training the generator
+        latent_space_samples = torch.randn((batch_size, 2))
+
+        #training the generator
+        generator.zero_grad()
+        generated_samples = generator(latent_space_samples)
+        output_discriminator_generated = discriminator(generated_samples)
+        loss_generator = loss_function(output_discriminator_generated, real_samples_labels)
+        loss_generator.backward()
+        optimizer_generator.step() 
+
+        # debug loss
+        if n == batch_size -1:
+            print(f"Epoch: {epoch} Loss D.: {loss_discriminator} Loss G.: {loss_generator}")
+        
+    # debug with some plot
+    generated_samples = generated_samples.detach()
+    plt.clf()
+    plt.plot(generated_samples[:, 0], generated_samples[:, 1], ".")
+    plt.show(block=False)
+    plt.pause(.1)
+    
+# check the samples obtained from the generator 
+latent_space_samples = torch.randn(100, 2)
+generated_samples = generator(latent_space_samples)
+
+# show plots for debugging
+plt.show()
